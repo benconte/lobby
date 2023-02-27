84e870ae505221379e7a0359a54bbae9e085b86e
Hirwa Auguste <benconte201@gmail.com>
2023-02-27 21:24:41 +0200

    remove middleware.ts

--- src/pages/booking/[id]/index.tsx
+++ src/pages/booking/[id]/index.tsx
@@ -1,40 +1,63 @@
 import Header from "@/components/Header"
 import Head from "next/head"
-import { useState, useContext } from "react"
-import Router from "next/router"
+import { useContext, useEffect } from "react"
 import { urlParams } from "@/utils/UrlOptions"
 import Details from '@/components/booking/details/Details'
 import Payment from '@/components/booking/Payment'
 import Tabs from '@/components/booking/Tabs'
 import Confirmation from '@/components/booking/Confirmation'
 import BookingContext, { BookContext } from "@/context/BookingContext"
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import { useSession } from 'next-auth/react';
+import Router from "next/router"
+
+const loading = require("@/assets/lottiefiles/loading.json")
 
 function Booking({ hotel }: { hotel: any }) {
     const { tabIndex } = useContext(BookContext)
-    return (
-        <BookingContext>
-            <div className='w-full pt-20'>
-                <Head>
-                    <title>Lobby - Booking</title>
-                    <meta name="description" content="A Hotel seaching and booking ap" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-                <Header />
+    const { status, data: session } = useSession()
 
-                <div className="w-full px-3 md:px-10 min-h-screen py-7 bg-gray-100 h-auto overflow-hidden">
-                    {/* breadcrumbs */}
-                    {/* <Tabs /> */}
-                    <div className="w-full mt-3">
-                        {/* {tabIndex === 1 && <Details hotel={hotel} />} */}
-                        {/* {tabIndex === 2 && <Payment />} */}
-                        {/* {tabIndex === 2 && <Confirmation />} */}
-                        <Details hotel={hotel} />
+    useEffect(() => {
+        if (status === "unauthenticated") Router.replace("/auth/signin"); // redirect to sign in not authenticated
+    }, [session, status])
+    if (status == "authenticated") {
+        return (
+            <BookingContext>
+                <div className='w-full pt-20'>
+                    <Head>
+                        <title>Lobby - Booking</title>
+                        <meta name="description" content="A Hotel seaching and booking ap" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1" />
+                        <link rel="icon" href="/favicon.ico" />
+                    </Head>
+                    <Header />
+
+                    <div className="w-full px-3 md:px-10 min-h-screen py-7 bg-gray-100 h-auto overflow-hidden">
+                        {/* breadcrumbs */}
+                        {/* <Tabs /> */}
+                        <div className="w-full mt-3">
+                            {/* {tabIndex === 1 && <Details hotel={hotel} />} */}
+                            {/* {tabIndex === 2 && <Payment />} */}
+                            {/* {tabIndex === 2 && <Confirmation />} */}
+                            <Details hotel={hotel} />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </BookingContext>
-    )
+            </BookingContext>
+        )
+    }
+
+    // return a loading state while we check for authentication 
+    return <div className="w-screen h-screen flex items-center justify-center ">
+        <Player
+            autoplay
+            loop
+            src={loading}
+            style={{ width: 300, height: 300 }}
+        >
+            <Controls visible={false} />
+        </Player>
+    </div>
 }
 
 export default Booking
--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -3,12 +3,19 @@ import { useSession } from 'next-auth/react';
 import Header from "@/components/Header"
 import Head from "next/head"
 import Hotel from "@/components/favorites/Hotel"
+import Router from "next/router"
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
+const loading = require("@/assets/lottiefiles/loading.json")
+
 
 function Favorites() {
   const { status, data: session } = useSession()
   const [hotels, setHotels] = useState<any>([])
 
   useEffect(() => {
+    if (status === "unauthenticated") Router.replace("/auth/signin"); // redirect to sign in not authenticated
+
     if (status === "authenticated") {
       fetch(`/api/favorites/${session?.user?.email}/getFavorites`, {
         method: "GET",
@@ -21,30 +28,46 @@ function Favorites() {
       })
     }
   }, [session, status])
-  return (
-    <div className="pt-20 w-full min-h-screen bg-gray-100">
-      <Head>
-        <title>Lobby - Favorites</title>
-        <meta name="description" content="A Hotel searching and booking ap" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
 
-      <div className="w-full my-5 text-center">
-        <h3 className="text-2xl text-[var(--blue)] font-medium">Favorites</h3>
-        <span className="text-gray-400 text-base">All your favorites in one place for you to review</span>
-      </div>
+  if (status == "authenticated") {
+    return (
+      <div className="pt-20 w-full min-h-screen bg-gray-100">
+        <Head>
+          <title>Lobby - Favorites</title>
+          <meta name="description" content="A Hotel searching and booking ap" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
 
-      {hotels && (
-        <div className="w-full px-3 md:px-10 py-3 grid grid-flow-row-dense grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-          {hotels.map((hotel: any, index: Number) => (
-            <Hotel hotel={hotel} key={index} />
-          ))}
+        <div className="w-full my-5 text-center">
+          <h3 className="text-2xl text-[var(--blue)] font-medium">Favorites</h3>
+          <span className="text-gray-400 text-base">All your favorites in one place for you to review</span>
         </div>
-      )}
-    </div>
-  )
+
+        {hotels && (
+          <div className="w-full px-3 md:px-10 py-3 grid grid-flow-row-dense grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+            {hotels.map((hotel: any, index: Number) => (
+              <Hotel hotel={hotel} key={index} />
+            ))}
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  return <div className="w-screen h-screen flex items-center justify-center ">
+    <Player
+      autoplay
+      loop
+      src={loading}
+      style={{ width: 300, height: 300 }}
+    >
+      <Controls visible={false} />
+    </Player>
+  </div>
+
+
 }
 
 export default Favorites
